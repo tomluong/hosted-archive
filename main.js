ece59720b1d860104b69d5d7199159858d809fac
Tom Luong <tomluong@live.com>
2021-08-05 00:21:12 -0400

    add search event

---
+++ main.js
@@ -0,0 +1,6 @@
+const DEV_WIKI_URL = 'https://www.google.com'
+var searchDevWiki = document.getElementById('search-dev-wiki');
+searchDevWiki.addEventListener('click', function (event) {
+    var searchInput = document.getElementById('search-input').value;
+    window.open(`${DEV_WIKI_URL}/?query=${encodeURI(searchInput)}`, '_blank');
+});
